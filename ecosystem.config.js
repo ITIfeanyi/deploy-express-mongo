2aed9a078a87e69bf9c79cebbf3651d7afb71b75
ITIfeanyi <ifeanyithankgodigweh@gmail.com>
2021-09-27 23:03:12 -0700

    initial commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,12 +9,12 @@ module.exports = {
   deploy: {
     production: {
       user: "ubuntu",
-      host: "",
-      ref: "",
-      repo: "",
-      path: "",
-      "pre-setup": "echo 'This is a pre-setup command",
-      "pre-deploy-local": "echo 'Thiz is a local executed command'",
+      host: "18.217.35.253",
+      ref: "origin/master",
+      repo: "https://github.com/ITIfeanyi/deploy-express-mongo.git",
+      path: "/home/ubuntu/deploy",
+      "pre-setup": "pwd",
+      "pre-deploy-local": "echo 'This is a local executed command'",
       "post-deploy":
         "cp ./.env ./ && npm install && pm2 startOrRestart ecosystem.config.js --env production",
     },
